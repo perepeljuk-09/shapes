99cef537441bd264be505083dc29c0ad6e107a9c
perepeljuk-09 <77107021+perepeljuk-09@users.noreply.github.com>
2023-10-25 18:54:09 +0300

    add sql

---
+++ shapes/SQL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shapes
+{
+	internal class SQL
+	{
+	}
+}
+
+// Второе задание
+/*	
+		SELECT p.name, c.name
+		FROM products p
+		LEFT JOIN products_categories pc ON pc.product_id = p.product_id
+		LEFT JOIN categories c ON pc.category_id = c.category_id
+*/
